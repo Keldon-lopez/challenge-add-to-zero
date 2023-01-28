41ad2f8a4a585c13546f0c597bc3e2857ebf7e49
Keldon Lopez <keldon.lopz@gmail.com>
2023-01-27 18:47:37 -0700

    provided a solution

--- addToZero.js
+++ addToZero.js
@@ -1,4 +1,17 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 12]
+let array = [28, 43, -12, 30, 4, 0, 12];
 
 // Write your solution below:
+const equalZero = (array) => {
+  let answer = false;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] === array[j]) {
+      } else if (array[i] + array[j] === 0) {
+        answer = true;
+      }
+    }
+  }
+  console.log(answer);
+};
+equalZero(array);
